e8bc4304fa76784e8bd0d64f84a936b340359d61
CArranz314 <132569169+CArranz314@users.noreply.github.com>
2024-03-17 19:46:59 +0100

    Update index.js

--- pages/index.js
+++ pages/index.js
@@ -4,17 +4,18 @@ import Footer from '@components/Footer'
 
 export default function Home() {
   console.log('12345678')
+  var aaaa=123434
   return (
     <div className="container">
       <Head>
         <title>Next.js Starter!</title>
         <link rel="icon" href="/favicon.ico" />
       </Head>
-
+            
       <main>
         <Header title="Welcome to my app!" />
         <p className="description">
-          Hola mundo
+          Hola mundo {{aaaa}}
         </p>
       </main>
 

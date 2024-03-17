ff653dfb4e32d49b10aa6a2c8ee53354cfc2d1ce
CArranz314 <132569169+CArranz314@users.noreply.github.com>
2024-03-17 20:07:02 +0100

    Update index.js

--- pages/index.js
+++ pages/index.js
@@ -1,14 +1,13 @@
 import Head from 'next/head'
 import Header from '@components/Header'
 import Footer from '@components/Footer'
-import { useParams } from 'next/navigation'
+
 
 
 export default function Home() {
   console.log('12345678')
   var aaaa=123434
-  const params = useParams<{ tag: string; item: string }>()
-  console.log(params)
+
   return (
     <div className="container">
       <Head>
